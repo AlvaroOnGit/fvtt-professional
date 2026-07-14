e908c85c3cd0930a8ebc8d8d7cee6590872a3c2e
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-07-14 18:38:11 +0200

    - Initialize localization helper - Initialize and link the DataModel for recipe item Subtypes - Swap the default icon for the item creation dialog - Register a new sheet for recipe item subtypes - Remove the default sheet from recipe item subtypes

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,59 @@
-Hooks.once('tidy5e-sheet.ready', () => {
-    const api = game.modules.get('tidy5e-sheet')?.api;
+import RecipeData from './data/recipeData';
+import RecipeSheet from "./sheets/recipeSheet";
 
-    if(!api) return;
+/**
+ * Global helper to localize keys
+ */
+(globalThis as any)._loc = (key: string): string => game.i18n.localize(key);
 
-    const craftingTab = new api.models.HtmlTab({
-          title: 'Professional.Crafting.Title',
-          tabId: "professional-crafting-tab",
-          html: `<button type="button" class="my-button">My button</button>`,
-    });
+const moduleId = 'professional';
+const sheetType = `${moduleId}.recipe`;
 
-    const gatheringTab = new api.models.HtmlTab({
-        title: 'Professional.Gathering.Title',
-        tabId: "professional-gathering-tab",
-        html: `<button type="button" class="my-button">My button</button>`,
-    });
+Hooks.once('init', () => {
+    CONFIG.Item.dataModels[`${moduleId}.recipe`] = RecipeData;
+    CONFIG.DND5E.defaultArtwork.Item[`${moduleId}.recipe`] = `modules/${moduleId}/assets/icons/recipe.svg`;
 
-    api.registerActorTab(craftingTab);
-    api.registerActorTab(gatheringTab);
-});
+    const Items = foundry.documents.collections.Items;
+
+    Items.registerSheet(
+        moduleId,
+        RecipeSheet,
+        {
+            types: [`${sheetType}`],
+            makeDefault: true
+        }
+    );
+})
+
+
+Hooks.once('ready', () => {
+    /**
+    * Removes the default dnd5e item sheet from the recipe item subtype
+    */
+    const sheets = CONFIG.Item.sheetClasses[`${sheetType}`];
+
+    if (sheets) {
+        delete sheets["dnd5e.ItemSheet5e"];
+    }
+})
+
+//Hooks.once('tidy5e-sheet.ready', () => {
+//    const api = game.modules.get('tidy5e-sheet')?.api;
+//
+//    if(!api) return;
+//
+//    const craftingTab = new api.models.HtmlTab({
+//          title: 'Professional.Crafting.Title',
+//          tabId: "professional-crafting-tab",
+//          html: `<button type="button" class="my-button">My button</button>`,
+//    });
+//
+//    const gatheringTab = new api.models.HtmlTab({
+//        title: 'Professional.Gathering.Title',
+//        tabId: "professional-gathering-tab",
+//        html: `<button type="button" class="my-button">My button</button>`,
+//    });
+//
+//    api.registerActorTab(craftingTab);
+//    api.registerActorTab(gatheringTab);
+//});
